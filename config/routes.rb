a3547ed780c105d8eb88ab2f55a73deaec0464fe
igarashijyri <fourleafclover688@gmail.com>
2023-10-05 23:40:00 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  # user_salaries
   resources :user_salaries, only: [:index]
   post '/user_salaries/monthly_detail', to: 'user_salaries#monthly_detail'
+
+  # devise
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
