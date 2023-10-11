e78c1a87cab7eaeb8f0c8c5bd912a5e030ea4c56
igarashijyri <fourleafclover688@gmail.com>
2023-10-11 23:59:50 +0900

    WIP: ajax対応中

--- app/controllers/pay_slips_controller.rb
+++ app/controllers/pay_slips_controller.rb
@@ -2,14 +2,17 @@ class PaySlipsController < ApplicationController
   def index
   end
 
-  # viewでテンプレートに変更が加えられないので原因調査する
-  # turbo falseにしたらCSRFトークン認証でエラー吐くようになった。
-  # Ajaxにするか普通のHTMLレスポンスにするかどうかを考える
+  # リクエスト送信時、そもそもPOSTじゃなくてGETの方が適切な気がしてきた
+  # (検索をしているだけでDBに更新などをかけているわけではないので)
   def monthly_detail
     year  = date_params["date(1i)"]
     month = date_params["date(2i)"]
     query_params = { :year_month => "#{year}-#{month}" }
     @pay_slip = PaySlip.fetch_record_for(query_params)
+
+    respond_to do |format|
+      format.html { render :index }
+    end
   end
 
   private
